4ea4d3c351a58bde2c8fd589e891d1ee3cff15ed
MbSmD <144994955+MbSmD@users.noreply.github.com>
2024-10-07 14:36:36 +0500

    Update main.c

--- лаба 2/main.c
+++ лаба 2/main.c
@@ -3,19 +3,31 @@
 
 int main()
 {
-    double x,p;
-    printf("vvedite PI s nujnbIm kolichesvom znakov ->");
-    scanf("%lf", &p);
-    printf("Enter x { 0.0 <= x <= 1.0 } -> ");
+    double x, y, z, p,S1,S2;
+    p = M_PI;
+    printf("vvedite x: ");
     scanf("%lf", &x);
-//функция sin(x) определена на отрезке от -1 до 1, но там корень так что трицательным оно не может быть
-    if (0.0 <= x && x <= 1.0) 
+    
+    
+    S1 = sin(2*x);
+    S2 = sin(3*x);
+    y = sqrt(sin(2*x))+sqrt(sin(3*x));
+    
+    if (S1 >= 0 && S2 >= 0 && sqrt(S1)+sqrt(S2) - p/8 >=0 && tan(sqrt(S1)+sqrt(S2) - p/8 >=0)) 
     {
-        double y = sqrt(sin(2*x)) +  sqrt(sin(3*x));
-        double z = pow(sqrt(log(tan(y - p/8))), 1.0/4.0);
-        printf("y(x) = %lf\nz(y) = %lf\n", y, z);
-    }
+        y = sqrt(sin(2*x))+sqrt(sin(3*x));
+        z = pow(sqrt(log(tan(y - p/8))), 1.0/4.0);
+        printf("y(x) = %lf\n z(y) = %lf\n", y, z);
+        return 0;
+        
+    } 
+   
     else
-        printf("x value is incorrect!\n");
-    return 0;
+    {
+        printf("oshibka,x ne podhodit\n"); 
+        return 1;
+        
+    }
+        
+
 }
